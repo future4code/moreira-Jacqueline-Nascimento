0ffeab650a74289045179191099dfa9b7558d4d9
Jacqueline Nascimento <jacqueline.cmessias@gmail.com>
2021-12-13 12:13:51 -0300

    Exercício 11 (Revisão JS - 13/12/21)

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -110,7 +110,7 @@ function retornaSegundoMaiorESegundoMenor(array) {
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-      
+    return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores.toString().replaceAll(",",", ")}.`  
 }
 
 // EXERCÍCIO 12
