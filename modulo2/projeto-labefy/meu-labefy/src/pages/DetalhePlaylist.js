88d0388536e0c84782f744a31cc54eb01ad0ca89
Jacqueline Nascimento <jacqueline.cmessias@gmail.com>
2022-01-31 17:00:54 -0300

    últimas atualizações de card

--- modulo2/projeto-labefy/meu-labefy/src/pages/DetalhePlaylist.js
+++ modulo2/projeto-labefy/meu-labefy/src/pages/DetalhePlaylist.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { ContainerDetalhe, ListMusic } from '../components/Styled';
 import { addTrackToPlaylist, getPlaylistTracks, removeTrackFromPlaylist } from '../services/integracoes';
 import lixo from '../assets/lixo.png';
+import { CardMusica } from '../components/CardMusica';
 
 const url = 'https://rr1---sn-hioxu-m52s.googlevideo.com/videoplayback?expire=1643418020&ei=RD30YeSQMK2JvdIPqsqKgAc&ip=154.12.96.125&id=o-AFd2Q6y2bALbT-4GMvb5Wcm40JuQmvMjIAtyIcUQA24o&itag=251&source=youtube&requiressl=yes&vprv=1&mime=audio%2Fwebm&gir=yes&clen=4301657&dur=268.661&lmt=1575239678519279&keepalive=yes&fexp=24001373,24007246&c=ANDROID&txp=5531432&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&sig=AOq0QJ8wRAIgBVmw8nja73rvFxJFdPEjk2BpOLXvGW0mySutV51mFU4CICbpRFwe21trQCvVIePO1GPnkyXkqlYK0iruNfoRrPND&title=Carmen%20-%20Habanera%20-%20Lyrics%20(Angela%20Gheorghiu)&redirect_counter=1&rm=sn-5hnelr7e&req_id=217c586b2fdba3ee&cms_redirect=yes&ipbypass=yes&mh=5N&mip=2804:108c:c8db:8386:82e9:e629:12d7:25d&mm=31&mn=sn-hioxu-m52s&ms=au&mt=1643395941&mv=u&mvi=1&pcm2cms=yes&pl=40&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pcm2cms,pl&lsig=AG3C_xAwRgIhANpbC0e5pDdoKd0Bpgd5YyTZEhMZkROfXHkFW5eUMPGRAiEAuwy3sen15cQW6fK7k0si7gvg8-xx5qkDfIBQIA4_QVQ%3D'
 
@@ -12,14 +13,17 @@ export default class DetalhePlaylist extends Component {
         inputNome: '',
         inputArtist: '',
         inputNumero: '',
+        deleteClicado: false,
+        addClicado: false,
     }
 
     componentDidMount(){
         getPlaylistTracks(this.props.id, this.saveLists)
     }
 
-    componentDidUpdate(){
+    componentDidUpdate(prevProps, prevState){
         getPlaylistTracks(this.props.id, this.saveLists)
+        
     }
     
     saveLists= (data) => {
@@ -46,23 +50,19 @@ export default class DetalhePlaylist extends Component {
         addTrackToPlaylist(this.state.inputNome, this.state.inputArtist, this.state.inputNumero, this.props.id)
         this.setState({inputNome: '',
                         inputArtist: '',
-                        inputNumero: '',})
+                        inputNumero: '',
+                        addClicado: !this.state.addClicado})
     }
 
     apagarMusica = (nome, idM, idP) =>{
         removeTrackFromPlaylist(idP, idM, nome)
+        this.setState({deleteClicado: !this.state.deleteClicado})
     }
 
     render() {
         const lista = this.state.listaMusicas.map((item)=>{
             // const url = item.url
-            return (<ListMusic key={item.id}>
-                <div><h2>{item.name}</h2><h3>{item.artist}</h3></div>
-                <button onClick={()=>this.apagarMusica(item.name,item.id,this.props.id)}>
-                    <img src={lixo} />
-                </button>
-                {item.url&&<audio controls src={url} />}
-                </ListMusic>)
+            return (<CardMusica apagarMusica={this.apagarMusica} key={item.id} item={item} id={this.props.id} />)
         })
         return (
         <ContainerDetalhe>
